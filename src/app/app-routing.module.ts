b12e80aa52608b03155d5fda645be199dfa68796
keita <kmahamadou858@gmail.com>
2023-01-15 16:03:24 +0100

    gestion de la page splash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 
 const routes: Routes = [
   // {
   //   path: '',
   //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   // },
-  {
-    path: '',
-    redirectTo: 'bienvenue',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'bienvenue',
+  //   pathMatch: 'full'
+  // },
   {
     path: 'bienvenue',
     loadChildren: () => import('./bienvenue/bienvenue.module').then( m => m.BienvenuePageModule)
   },
-  {
-    path: 'splash',
-    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
-  },
+
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashComponent },
 
 ];
 @NgModule({
